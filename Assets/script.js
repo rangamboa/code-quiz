3081e8aa9350dccebf53147f2fae37108acfc49d
rangamboa <rangamboa@gmail.com>
2021-06-29 15:52:41 -0700

    cleaning up JS code to optimize function flow

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // Declare global variables.
-var count = 0 ;
-var timer = 10;
+var count = 0;
+var timer = 75;
+var timeInterval;
 var score;
 var highScores;
 var choices;
@@ -16,14 +17,14 @@ var quizEl = document.querySelector('#quizDiv');
 
 // Declare multiple-choice questions and corresponding answers in respective arrays.
 // The last element in the array signifies the position of the right answer.
-var q1 = ['What color is the sky?', 'blue', 'green', 'red', 'polka dots', '1'];
-var q2 = ['What color are my eyes?', 'blue', 'green', 'red', 'brown', '4'];
-var q3 = ['What color is my hair?', 'blue', 'black', 'red', 'purple', '2'];
-var q4 = ['What is your favorite pet?', 'dog', 'shark', 'sloth', 'platypus', '1'];
-var q5 = ['What color is your car?', 'purple', 'green', 'white', 'grey', '3'];
+var q0 = ['1. What color is the sky?', 'blue', 'green', 'red', 'polka dots', '1'];
+var q1 = ['2. What color are my eyes?', 'blue', 'green', 'red', 'brown', '4'];
+var q2 = ['3. What color is my hair?', 'blue', 'black', 'red', 'purple', '2'];
+var q3 = ['4. What is your favorite pet?', 'dog', 'shark', 'sloth', 'platypus', '1'];
+var q4 = ['5. What color is your car?', 'purple', 'green', 'white', 'grey', '3'];
 
 // Combine all arrays into a single large array.
-var questionsArray = [q1, q2, q3, q4, q5];
+var questionsArray = [q0, q1, q2, q3, q4];
 
 function init() {
 
@@ -32,59 +33,35 @@ function init() {
     quizEl.setAttribute('class', 'hidden');
     timerEl.textContent = timer;
 
-    console.log(count);
-    console.log(questionsArray);
 }
 
 function displayQ () {
 
     // Cycle through all questions in array.
-    do {
-            
+
+    if (count < questionsArray.length) {
+
         quizEl.innerHTML = '<h1>' + questionsArray[count][0] + '</h1>';
 
         // Cycle through all multiple choice answers in each subarray.
-        for (j = 1; j < questionsArray[count].length-1; j++) {
+        for (i = 1; i < questionsArray[count].length-1; i++) {
 
             // Build IDs for answer buttons.
-            var buildBtnID = 'answer-' + j;
+            var buildBtnID = 'answer-' + i;
             var btnClass = 'multiChoice';
 
             // Build the HTML for the actual buttons.
-            var buildBtn = '<button id='+ buildBtnID +' class='+ btnClass +'>' + j + '. ' + questionsArray[count][j] + '</button><br />';
+            var buildBtn = '<button id='+ buildBtnID +' class='+ btnClass +'>' + i + '. ' + questionsArray[count][i] + '</button><br />';
 
             // Add the buttons to the div.
             quizEl.innerHTML += buildBtn;
         };
-    
-        // Listen for clicks on any multiple choice button.
-        quizEl.addEventListener('click', function(event) {
-
-            // Pull the first value of the string. This corresponds to its position in the array.
-            var firstChar = event.target.textContent.charAt(0);
-            console.log(firstChar);
-            console.log(questionsArray[count][5]);
-
-            // Compare value of selected string vs. value of correct answer.
-            if (firstChar == questionsArray[count][5]) console.log('Right answer!');
-            else {
-                console.log('Wrong answer!');
-                timer-=10;
-            }
-        });
-        count++;
-        quizEl.innerHTML = '';
+    } else {
+        console.log('no more questions');
+        clearInterval(timeInterval);
+        console.log(timer);
+        quizEl.setAttribute('class', 'hidden');
     }
-
-    while (count < questionsArray.length);
-
-    // Go to the next question in array.
-  
-    // quizEl.innerHTML = '';
-
-    // Call function again.
-    // displayQ();
-
 }
 
 // Listen for a mouse click on the 'Begin Quiz' button.
@@ -92,22 +69,20 @@ beginBtn.addEventListener('click', function(event) {
 
     event.preventDefault();
 
-    console.log('Begin Quiz button clicked');
-
     // Hide the div containing the introductory text.
     introEl.className = 'hidden';
 
     // Unhide quizDiv (div containing the quiz questions).
-        quizEl.classList.remove('hidden');
+    quizEl.classList.remove('hidden');
 
-    // Start a timer counting every 1 second.
-    var timeInterval = setInterval(function() {
+    // Start a timer to display a countdown every 1 second.
+    timeInterval = setInterval(function() {
     
         // Decrements and displays the remaining time.
         timer--;
         timerEl.textContent = timer;
     
-        if (timer == 0) {
+        if (timer <= 0) {
             console.log('TIME IS UP SUCKAAAA');
             clearInterval(timeInterval);
             quizEl.setAttribute('class', 'hidden');
@@ -119,5 +94,26 @@ beginBtn.addEventListener('click', function(event) {
     displayQ();
 });
 
+// Listen for clicks on the dynamically-generated multiple choice buttons.
+quizEl.addEventListener('click', function(event) {
+
+    // Pull the first value of the string. This corresponds to its position in the array.
+    var firstChar = event.target.textContent.charAt(0);
+
+    console.log(firstChar + ' and ' + questionsArray[count][5]);
+            
+    // Compare value of selected string vs. value of correct answer.
+    if (firstChar == questionsArray[count][5]) {
+        console.log('Right answer!');
+    } else {
+        console.log('Wrong answer!');
+        timer-=10;
+    }
+
+    // Displays the next question regardless of right/wrong answer.
+    count++;
+    displayQ();
+});
+ 
 // Launch function to initialize application.
 init();
