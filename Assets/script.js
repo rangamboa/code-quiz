b7e95c8cee3d36ea2aa61c6947f02123f630e02d
rangamboa <rangamboa@gmail.com>
2021-06-29 17:57:01 -0700

    testing multiple choice questions

--- Assets/script.js
+++ Assets/script.js
@@ -15,11 +15,11 @@ var statusEl = document.querySelector('#status')
 
 // Declare multiple-choice questions and corresponding answers in respective arrays.
 // The last element in the array signifies the position of the right answer.
-var q0 = ['1. What color is the sky?', 'blue', 'green', 'red', 'polka dots', '1'];
-var q1 = ['2. What color are my eyes?', 'blue', 'green', 'red', 'brown', '4'];
-var q2 = ['3. What color is my hair?', 'blue', 'black', 'red', 'purple', '2'];
-var q3 = ['4. What is your favorite pet?', 'dog', 'shark', 'sloth', 'platypus', '1'];
-var q4 = ['5. What color is your car?', 'purple', 'green', 'white', 'grey', '3'];
+var q0 = ['1. Which built-in method returns the length of the string?', 'length()', 'size()', 'index()', 'toString()', '1'];
+var q1 = ['2. JavaScript is a _____-side programming language.', 'client', 'server', 'both', 'none', '3'];
+var q2 = ['3. What is the correct "if" statement to execute code if "x" is equal to 2?', 'if (x 2)', 'if (x = 2)', 'if (x == 2)', 'if (x != 2)', '3'];
+var q3 = ['4. Which of the following is NOT a valid function call?', 'var x = myFunc()', 'myFunc', 'x = myFunc()', 'myFunc()', '2'];
+var q4 = ['5. Which is an advantage of using JavaScript?', 'less server interaction', 'immediate feedback to visitors', 'increased interactivity', 'all of the above', '4'];
 
 // Combine all arrays into a single large array.
 var questionsArray = [q0, q1, q2, q3, q4];
@@ -29,7 +29,6 @@ function init() {
     // (Re)set the screen to play.
     introEl.classList.remove('hidden');
     quizEl.setAttribute('class', 'hidden');
-    // statusEl.setAttribute('class', 'hidden');
     timerEl.textContent = timer;
 }
 
@@ -70,8 +69,6 @@ function displayQ () {
     }
 }
 
-
-
 // Listen for a mouse click on the 'Begin Quiz' button.
 beginBtn.addEventListener('click', function(event) {
 
@@ -91,10 +88,12 @@ beginBtn.addEventListener('click', function(event) {
         timerEl.textContent = timer;
     
         if (timer <= 0) {
-            console.log('TIME IS UP SUCKAAAA');
             clearInterval(timeInterval);
-            quizEl.setAttribute('class', 'hidden');
-            statusEl.setAttribute('class', 'hidden');
+            // console.log('TIME IS UP SUCKAAAA');
+
+            // go to high scores function.
+            // quizEl.setAttribute('class', 'hidden');
+            // statusEl.setAttribute('class', 'hidden');
             return;
         }
     }, 1000);
@@ -109,11 +108,11 @@ quizEl.addEventListener('click', function(event) {
     // Pull the first value of the string. This corresponds to its position in the array.
     var firstChar = event.target.textContent.charAt(0);
           
-    // Compare value of selected string vs. value of correct answer.
+    // Compare value of selected character vs. value of correct answer.
     if (firstChar == questionsArray[count][5]) {
-        message = 'YASSSSSS';
+        message = 'Right!';
     } else {
-        message = 'NOOOOOOO!';
+        message = 'Wrong!';
         timer-=10;
         timerEl.textContent = timer;
     }
